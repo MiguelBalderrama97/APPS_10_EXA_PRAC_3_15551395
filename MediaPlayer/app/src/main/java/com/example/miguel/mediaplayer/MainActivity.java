91af5bf47d36df82ed2b78063ed3abe5baf219e6
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-05-21 22:27:38 -0600

    Layout y componentes vinculados

--- MediaPlayer/app/src/main/java/com/example/miguel/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/miguel/mediaplayer/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.miguel.mediaplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private TextView txtName;
+    private ImageButton btnPrev, btnNext, btnPlay, btnPause;
+    private Switch swRandom;
+    private ImageView imgIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtName = findViewById(R.id.txtName);
+        btnPrev = findViewById(R.id.btnPrev);
+        btnNext = findViewById(R.id.btnNext);
+        btnPlay = findViewById(R.id.btnPlay);
+        btnPause = findViewById(R.id.btnPause);
+        swRandom = findViewById(R.id.swRandom);
+        imgIcon = findViewById(R.id.imgIcon);
     }
 }
